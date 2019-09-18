41bcb8f6e03496cad48fbbbecdf8f223f69c6179
Istvan Demeter <demeter.istvan@gmail.com>
2019-09-18 12:53:39 +0200

    gzip test

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ $router->get('/big-without-headers', function () use ($router) {
     ]);
     return $response->send();
 });
+
 $router->get('/big-with-headers', function () use ($router) {
     $response = new StreamedResponse(function () {
         $ticks = 45;
@@ -61,4 +62,20 @@ $router->get('/big-with-headers', function () use ($router) {
     $response->headers->set('X-Accel-Buffering', 'no');
 
     return $response->send();
+});
+
+$router->get('/big-not-streamed', function () use ($router) {
+    $data = [];
+    for ($index = 0; $index < 45; $index++) {
+        $data[] = ['name' => 'bla', 'index' => $index, 'a' => array_fill(1, 5000, rand())];
+    }
+    return response()->json($data);
+});
+
+$router->get('/big-not-streamed-with-headers', function () use ($router) {
+    $data = [];
+    for ($index = 0; $index < 45; $index++) {
+        $data[] = ['name' => 'bla', 'index' => $index, 'a' => array_fill(1, 5000, rand())];
+    }
+    return response()->json($data)->withHeaders(['X-Accel-Buffering' => 'no']);
 });
